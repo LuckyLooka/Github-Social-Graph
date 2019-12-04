e3cfd39710ba7673157981a0d9b53f45df13bca5
LuckyLooka <hickeylu@tcd.ie>
2019-12-04 22:46:41 +0000

    added github_graph, incomplete

---
+++ github_graph/src/components/StarGraph.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+const SortedList = (props) => {
+  if (props.repitems) {
+    return (
+        <ul>
+          {
+           {props.repitems.map((repitem) =>
+            
+            // <li key={repitem.sha}>
+            //   <div>
+            //     <div>
+            //        Commit by "{repitem.commit.author.name}" at {repitem.commit.author.date}
+            //     </div>
+            //     <div>
+            //       <i>Commit message :  {repitem.commit.message}</i>
+            //     </div>
+            //   </div>
+            // </li>
+
+          )} 
+
+          }
+        </ul>
+      )
+  } else { return null;}
+  };export default SortedList;
