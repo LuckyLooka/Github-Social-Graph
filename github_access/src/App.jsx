166b60f6f7dc73df6569f7f2debbc293ea4f15d9
LuckyLooka <hickeylu@tcd.ie>
2019-12-04 18:42:18 +0000

    added authorisation

--- github_access/src/App.jsx
+++ github_access/src/App.jsx
@@ -11,13 +11,13 @@ class App extends Component {
     }
     this.handleClick = this.handleClick.bind(this)
   }
-
+//8d446116c177c98ac8b1936640e487d8c0802d10
 handleClick(e) {
-  const username = "LuckyLooka"
-  con
-    axios.get('https://api.github.com/users/LuckyLooka')
+    let token = '8d446116c177c98ac8b1936640e487d8c0802d10';
+    axios.get('https://api.github.com/users/LuckyLooka/repos', {headers:{"Authorization" : `Bearer ${token}`} })
     .then(response => this.setState({
-      username: response.data.login,
+      username: response.data.title,
+      repoNames: response.data.name,
       info : JSON.stringify(response.data, undefined, 2)
     }));
   }
@@ -26,14 +26,15 @@ render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">GitHub Analytics</h1>
+          <h1 className="App-title">GitHub Profile Details</h1>
         </header>
         <p className="App-intro">
-          Watch this space...
+          Profile Repositories
         </p>
         <Button handleClick={this.handleClick}/>
         <p><b>Username:</b></p>
         <p>{this.state.username}</p>
+        <p> {this.state.repoNames}</p>
         <b>Information:</b>
         <pre>{this.state.info}</pre>
       </div>
